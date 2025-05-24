82ef93b093faccf51a07607823bb1c3aada30744
ManjiriAhire12 <101831435+ManjiriAhire12@users.noreply.github.com>
2025-05-24 14:54:12 +0530

    Add files via upload

---
+++ ExpenseTypeProgram.java
@@ -0,0 +1,35 @@
+package ExpenseType;
+
+public class ExpenseTypeProgram {
+
+
+	
+	public static int dinner(int a)
+	{
+		System.out.println("Expense Type of dinner is " + a);
+		return a;
+	}
+	public static int  breakfast (int b)
+	{
+		System.out.println("Expense type of breakfast is :" +b);
+		return b;
+	}
+	public static int carRental(int c)
+	{
+		System.out.println("Car rental is : "+ c);
+		return c;
+	}
+	public static  int mealExpense(int d)
+	{
+	System.out.println("The meal expense is :" );
+	return d;
+	
+	}
+	
+	public static int lunch (int l)
+	{
+		
+		return l;
+	}
+
+}
